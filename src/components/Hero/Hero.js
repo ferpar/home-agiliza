9dbc0bcbdc5fb5da56758bf93a1d503cc718f47a
Fer <ferp@protonmail.ch>
2024-03-04 11:27:56 +0100

    use next image for improved loading

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import { getString } from "../../helpers/i18n";
 import styles from "./Hero.module.css";
+
 
 const title = getString("TITLE");
 const subtitle = getString("SUBTITLE");
@@ -20,10 +22,12 @@ export default function Hero() {
           <p>{description}</p>
         </div>
         <div className={styles["background"]}>
-          <img
+          <Image
             src="/images/Cotizaciones.png"
             alt="hero"
             className={styles["hero-img"]}
+            width={936}
+            height={422}
           />
         </div>
       </section>
