3f053360ff9e2a15c8b4e8cd052a9a7950d5b43f
Pedro Perez de Ayala <ppayala@gmail.com>
2024-03-22 11:14:59 +0100

    Integrate Tawk.to widget

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 import { getString } from "../helpers/i18n";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import Script from "next/script";
 
 const montserrat = Saira({
   subsets: ["latin"],
@@ -34,6 +35,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${ptSerif.variable} ${montserrat.variable}`}>
+      <Script id="tawk" type="text/javascript">
+        {`      
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/65fd52fb1ec1082f04da180f/1hpiooa9d';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+      `}
+      </Script>
       <body>
         <NavProvider>
           <Theme>
