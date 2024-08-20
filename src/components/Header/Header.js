b3b0b15e5a4d524dc139e2d67dfa3ca3b93a770a
Fer <ferp@protonmail.ch>
2024-08-20 13:44:54 +0200

    fix externalPage discriminator

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -40,21 +40,28 @@ function Header({ path, dict }) {
   // when rendering on the server, use the path from the request headers
   const actualPath = currentPath ? currentPath : path;
 
-  const externalPage = headerLinks.find((link) => link.path === actualPath)
+  const externalPage = headerLinks.find((link) => {
+    return (
+      link.path === actualPath ||
+      link.pathES === actualPath ||
+      link.pathEN === actualPath ||
+      link.pathLang === actualPath
+    );
+  })
     ? false
     : true;
 
   const darkTheme = externalPage
     ? true
     : headerLinks.find((link) => {
-      console.log({
-        link,
-        actualPath,
-        cond1: link.path === actualPath,
-        cond2: link.pathES === actualPath,
-        cond3: link.pathEN === actualPath,
-        cond4: link.pathLang === actualPath,
-      })
+        console.log({
+          link,
+          actualPath,
+          cond1: link.path === actualPath,
+          cond2: link.pathES === actualPath,
+          cond3: link.pathEN === actualPath,
+          cond4: link.pathLang === actualPath,
+        });
         return (
           link.path === actualPath ||
           link.pathES === actualPath ||
@@ -63,7 +70,7 @@ function Header({ path, dict }) {
         );
       })?.darkTheme;
 
-    console.log("Header.js: actualPath: ", {actualPath, darkTheme});
+  console.log("Header.js: actualPath: ", { actualPath, darkTheme });
 
   return (
     <>
