b276513e0db262d1cbe8189b5673af81c5ab7a70
Fer <ferp@protonmail.ch>
2024-09-16 12:09:25 +0200

    replace unreliable <br/> spacing

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,21 +5,17 @@ export default function About({ dict }) {
   return (
     <section className={styles["about"]}>
       <div className={styles["wrapper"]}>
-        <div className={styles["text"]}>
+        <div className={styles["text"]} style={{paddingBottom: "2rem"}}>
           <p className={styles["mainText"]}>
             <strong>{dict["TITLE"]}</strong>
             {dict["ABOUT_1"]}
           </p>
-          <br />
           <p style={{ fontSize: "2rem" }}>
             {dict["ABOUT_2"]}
           </p>
-          <br />
         </div>
         <FeatureArray dict={dict}/>
-        <br />
-        <br />
-        <p style={{ fontSize: "2rem" }}>
+        <p style={{ fontSize: "2rem", paddingTop: "2rem" }}>
           {dict["ABOUT_3"]}
         </p>
       </div>
