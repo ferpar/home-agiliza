3c7243d51e057b2d8aaf3818331ffc2b899bf47c
Fer <ferp@protonmail.ch>
2024-09-17 16:32:44 +0200

    add image backgrounds to module selectors

--- src/app/[lang]/app/ModuleSelector/Modules.js
+++ src/app/[lang]/app/ModuleSelector/Modules.js
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { Card } from "@radix-ui/themes";
 import QuotesModule from "./Quotes/QuotesModule";
 import RestockModule from "./Restock/RestockModule";
@@ -24,6 +25,14 @@ export default function AppModules({ dict }) {
           onClick={() => handleModuleSelection("quotes")}
         >
           <h3>{dict["QUOTES_MODULE"]}</h3>
+          <div className={styles.imgWrapper}>
+            <Image
+              src="/assets/quotes_S.jpg"
+              alt={dict["QUOTES_MODULE"]}
+              width={200}
+              height={200}
+            />
+          </div>
         </Card>
         <Card
           variant="ghost"
@@ -31,6 +40,14 @@ export default function AppModules({ dict }) {
           onClick={() => handleModuleSelection("restock")}
         >
           <h3>{dict["RESTOCK_MODULE"]}</h3>
+          <div className={styles.imgWrapper}>
+            <Image
+              src="/assets/CFRestock_S.jpg"
+              alt={dict["QUOTES_MODULE"]}
+              width={200}
+              height={200}
+            />
+          </div>
         </Card>
       </div>
       {selectedModule === "quotes" && <QuotesModule dict={dict} />}
